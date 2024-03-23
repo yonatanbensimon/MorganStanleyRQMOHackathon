ab62a138788c6ebee3b0939e91466def0d0083f3
tmir <taahaa.mir@mail.mcgill.ca>
2024-03-23 17:23:57 -0400

    Service initial commit

---
+++ frontend/my-app/src/gameService.js
@@ -0,0 +1,41 @@
+
+export function init(options) {
+    /*
+    *   Function called when game starts. Initialize the disease.
+    *   Update availableOptions array to add beginning options.
+    *   Set totalHours to zero. Call change view with the appropriate
+    *   parameter.
+    *
+    *   @return {String} - The instructions text.
+    */
+  }
+  
+  export function updateOptions(options, chosenOption) {
+    /*
+    *  Function called when a user selects an option. Update the
+    *  availableOptions array to reflect the new options. Update
+    *  the totalHours. If choseOption is a winning option, call
+    *  results().
+    * 
+    *  @return {String} - The dialogue of the chosenOption.
+    */
+  }
+  
+  export function results() {
+    /*
+    *   Function called when the user wins the game. Call changeView
+    *   with the appropriate parameter.
+    * 
+    *   @return {String} - The results of the game
+    */
+  }
+  
+  export function changeView(requestedView) {
+    /*
+     * Update the view to match the one in the parameter.
+     * 
+     * @param {String} requestedView - The view to change to.
+     * @return {None} 
+     */
+  }
+  
