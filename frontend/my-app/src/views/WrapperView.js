bb4bd51f9ac85b6a2f382089634ac878b26d1b0f
tmir <taahaa.mir@mail.mcgill.ca>
2024-03-24 06:29:40 -0400

    Implemented Service Functions

--- frontend/my-app/src/views/WrapperView.js
+++ frontend/my-app/src/views/WrapperView.js
@@ -4,11 +4,58 @@ import './WrapperView.css';
 import GameView from "./GameView/GameView";
 import ResultView from "./ResultView/ResultView"
 import { useState } from "react";
+import Story from './story.json';
 
 
 
 function WrapperView() {
-  const [currentView, setCurrentView] = useState("home");
+  const [currentView, setCurrentView] = useState("game");
+  const [weeks, setWeeks] = useState(0);
+  const [availableOptions, setAvailableOptions] = useState([]);
+
+
+  /*
+  *   Function called when game starts. Initialize the disease.
+  *   Update availableOptions array to add beginning options.
+  *   Set `weeks` to zero. Call change view with the appropriate
+  *   parameter.
+  *
+  *   @return {String} - The instructions text.
+  */
+  function init() {
+    setAvailableOptions([Story.NewOptions]);
+    setCurrentView("game");
+    setWeeks(0);
+    return 'placeholder';
+  }
+
+  /*
+  *  Function called when a user selects an option. Update the
+  *  availableOptions array to reflect the new options. Update
+  *  the weeks. If choseOption is a winning option, call
+  *  results().
+  * 
+  *  @return {String} - The dialogue of the chosenOption.
+  */
+  function updateOptions(chosenOption) {
+    const gameResult = Story?.chosenOption?.gameresult ?? "defaultResult";
+    if (gameResult) {
+      return results();
+    }
+    setAvailableOptions(Story.chosenOption.NewOptions);
+    setWeeks(weeks + Story.chosenOption.weeks);
+  }
+
+  /*
+  *  Function called when the user wins the game. Call changeView
+  *  with the appropriate parameter.
+  * 
+  *  @return {String} - The results of the game
+  */
+  function results() {
+    setCurrentView("result");
+    return 'placeholder';
+  }
   
   const renderView = () => {
     switch (currentView) {
