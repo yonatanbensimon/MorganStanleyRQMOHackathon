118a726843d690211f8f48aa0d83e331fbd4c747
Arthur <56365708+ChubbyWaddlingPenguin@users.noreply.github.com>
2024-03-24 21:31:56 +0000

    ResultView FIRE CONFETTI WOOOOOO

--- frontend/my-app/src/views/ResultView/ResultView.js
+++ frontend/my-app/src/views/ResultView/ResultView.js
@@ -1,30 +1,26 @@
 import React from 'react';
-import './ResultView.css'; // Import your CSS file for styling
+import './ResultView.css';
 import BestButton from '../../components/BestButton';
+import Confetti from 'react-confetti';
 
-const ResultView = ({ score = 100 }) => {
+const ResultView = ({ score = 100, success = true }) => {
   return (
     <div className="result-container">
-      <header>
-        <div className="header-left">
-          <button>Button 1</button>
-          <button>Button 2</button>
-        </div>
-        <h1 className="header-title">My Awesome Website</h1>
-        <div className="header-right">
-          <button>Button 3</button>
-          <button>Button 4</button>
-        </div>
-      </header>
       <div className="content">
-        <h2>Your Score</h2>
-        <div className="score">{score}</div>
-        <BestButton>Restart Game</BestButton>
-        <BestButton>Learn More</BestButton>
+        {success && (
+          <div className="confetti-container">
+            <Confetti width={800} height={600} numberOfPieces={85} opacity={0.7}/>
+          </div>
+        )}
+        <h2 style={{ color: success ? '#FFA500' : '#8B0000', fontSize: '50px' }}>{success ? 'Congratulations!' : 'Oh No!'}</h2>
+        <div className="score">Your Score: {score}</div>
+        <div className="button-container">
+          <BestButton>Restart Game</BestButton>
+          <BestButton>Learn More</BestButton>
+        </div>
       </div>
-      <footer>Footer things</footer>
     </div>
   );
-}
+};
 
 export default ResultView;
