53dd4add3f2046771a2c063f711ec56bed00bda1
Arthur <56365708+ChubbyWaddlingPenguin@users.noreply.github.com>
2024-03-24 21:42:30 +0000

    Confetti Fix

--- frontend/my-app/src/views/ResultView/ResultView.js
+++ frontend/my-app/src/views/ResultView/ResultView.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './ResultView.css';
+import './ResultView.css'; // Import your CSS file for styling
 import BestButton from '../../components/BestButton';
 import Confetti from 'react-confetti';
 
-const ResultView = ({ score = 100, success = true }) => {
+const ResultView = ({ score = 100, success = false, months = 1 }) => {
   return (
     <div className="result-container">
       <div className="content">
@@ -12,8 +12,10 @@ const ResultView = ({ score = 100, success = true }) => {
             <Confetti width={800} height={600} numberOfPieces={85} opacity={0.7}/>
           </div>
         )}
-        <h2 style={{ color: success ? '#FFA500' : '#8B0000', fontSize: '50px' }}>{success ? 'Congratulations!' : 'Oh No!'}</h2>
-        <div className="score">Your Score: {score}</div>
+        <h2 style={{ color: success ? '#FFA500' : '#8B0000', fontSize: '50px', marginBottom: '-5px', marginTop: '-10px'}}>{success ? 'Congratulations!' : 'Oh No!'}</h2>
+        <h2 style={{ color: success ? '#FFA500' : '#8B0000', fontSize: '25px', marginBottom: '25px' }}>{success ? 'Patient is recovering!' : 'Wrong Diagnosis!'}</h2>
+        <div className="diagnosistime">Your diagnosis took {months} months</div>
+        <div className="score">Score: {score}</div>
         <div className="button-container">
           <BestButton>Restart Game</BestButton>
           <BestButton>Learn More</BestButton>
