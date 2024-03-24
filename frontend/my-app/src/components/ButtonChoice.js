e095326f6b953d2cd95075b5cf484561a9abeeaa
exisodd <ethantran004@yahoo.ca>
2024-03-23 21:06:36 -0400

    added available choices options state in game view

--- frontend/my-app/src/components/ButtonChoice.js
+++ frontend/my-app/src/components/ButtonChoice.js
@@ -2,10 +2,11 @@
 import React from 'react';
 import './ButtonChoice.css'; // Importing the styles
 
-const ButtonChoice = ({ src, onClick }) => {
+const ButtonChoice = ({ src, onClick, children }) => {
   return (
     <button className="image-button" onClick={onClick}>
       <img src={src} />
+      {children}
     </button>
   );
 };
--- frontend/my-app/src/views/GameView/GameView.js
+++ frontend/my-app/src/views/GameView/GameView.js
@@ -7,12 +7,34 @@ import blood from "../../assets/blood.png"
 import antibiotics from "../../assets/antibiotics.png"
 
 function GameView({ changeView }) {
+
+    // Available options
+    const buttonChoices = [
+        {
+            src: mri,
+            onClick: () => changeView("home"), // Example for changeView usage
+            text: "MRI Scan"
+        },
+        {
+            src: blood,
+            onClick: () => { /* Define onClick handler for this button */ },
+            text: "Blood test"
+        },
+        {
+            src: antibiotics,
+            onClick: () => { /* Define onClick handler for this button */ },
+            text: "Antibiotics"
+        }
+    ];
+
     return (
         <div>
             <ButtonContainer>
-                <ButtonChoice src={mri} onClick={() => changeView("home")}/>
-                <ButtonChoice src={blood} onClick={NaN}/>
-                <ButtonChoice src={antibiotics} onClick={NaN}/>
+                {buttonChoices.map((choice, index) => (
+                    <ButtonChoice key={index} src={choice.src} onClick={choice.onClick}>
+                        {choice.text}
+                    </ButtonChoice>
+                ))}
             </ButtonContainer>
            <TextBox message={"This is a test"}/>
         </div>
