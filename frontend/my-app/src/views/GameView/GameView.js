02d209a5b6177e70371bc2bf2076f4b1680dc829
exisodd <ethantran.mail@gmail.com>
2024-03-24 20:46:19 -0400

    story fix

--- frontend/my-app/src/views/GameView/GameView.js
+++ frontend/my-app/src/views/GameView/GameView.js
@@ -14,6 +14,7 @@ function GameView({ changeView }) {
         "BloodTest": {
             "title": "Blood test",
             "src": blood,
+            onClick: () => changeView("home"),
             "dialogue": "There is nothing conclusive but indicates that scans might lead to something.",
             "newOptions": ["MRIScan", "CTScan"],
             "weeksTaken": 2
@@ -21,6 +22,7 @@ function GameView({ changeView }) {
         "MRIScan": {
             "title": "MRI scan",
             "src": mri,
+            onClick: () => changeView("home"),
             "dialogue": "It didn’t show anything abnormal.",
             "newOptions": null,
             "weeksTaken": 1,
@@ -29,6 +31,7 @@ function GameView({ changeView }) {
         "CTScan": {
             "title": "CT scan",
             "src": doctor,
+            onClick: () => changeView("home"),
             "dialogue": "No conclusions could be drawn.",
             "newOptions": null,
             "weeksTaken": 1,
@@ -37,6 +40,7 @@ function GameView({ changeView }) {
         "Antibiotics": {
             "title": "Antibiotics",
             "src": antibiotics,
+            onClick: () => changeView("home"),
             "dialogue": "I tried them for a few months, but there were no improvements in my condition.",
             "newOptions": null,
             "weeksTaken": 12,
@@ -45,6 +49,7 @@ function GameView({ changeView }) {
         "Neurologist": {
             "title": "Neurologist",
             "src": doctor,
+            onClick: () => changeView("home"),
             "dialogue": "\"Sorry, I couldn’t pinpoint the issue. You should give a rheumatologist or a genetic testing a try.\"",
             "newOptions": ["GeneticTesting", "Rheumatologist"],
             "weeksTaken": 3
@@ -52,6 +57,7 @@ function GameView({ changeView }) {
         "GeneticTesting": {
             "title": "Genetic testing",
             "src": doctor,
+            onClick: () => changeView("home"),
             "dialogue": "The test showed that my genetics are susceptible to cancer, I might learn more with an oncologist.",
             "newOptions": ["Oncologist"],
             "weeksTaken": 2
@@ -59,6 +65,7 @@ function GameView({ changeView }) {
         "Oncologist": {
             "title": "Oncologist",
             "src": doctor,
+            onClick: () => changeView("home"),
             "dialogue": "“I don’t know what you have. There is no sign of cancer. I’m sorry I can’t help you more.”",
             "newOptions": null,
             "weeksTaken": 4,
@@ -67,6 +74,7 @@ function GameView({ changeView }) {
         "Rheumatologist": {
             "title": "Rheumatologist",
             "src": doctor,
+            onClick: () => changeView("home"),
             "dialogue": "“This is out of my specialty. I can refer you to a trial treatment. Also, I suggest an endoscopy.”",
             "newOptions": ["TrialTreatment", "Endoscopy"],
             "weeksTaken": 3
@@ -74,6 +82,7 @@ function GameView({ changeView }) {
         "TrialTreatment": {
             "title": "Trial treatment",
             "src": doctor,
+            onClick: () => changeView("home"),
             "dialogue": "I tried the treatment for a year, but it didn't help my condition whatsoever.",
             "newOptions": null,
             "weeksTaken": 52,
@@ -82,6 +91,7 @@ function GameView({ changeView }) {
         "Endoscopy": {
             "title": "Endoscopy",
             "src": doctor,
+            onClick: () => changeView("home"),
             "dialogue": "It shows that my lungs might have an issue, so I could check a pulmonologist.",
             "newOptions": ["Pulmonologist"],
             "weeksTaken": 2
@@ -89,6 +99,7 @@ function GameView({ changeView }) {
         "Pulmonologist": {
             "title": "Pulmonologist",
             "src": doctor,
+            onClick: () => changeView("home"),
             "dialogue": "“Your lungs aren’t functioning properly. I believe you have a condition where each lung acts independently from the other during sleep. This would explain the symptoms that you have. We found the crux of the issue.”",
             "newOptions": null,
             "weeksTaken": 6,
@@ -97,24 +108,7 @@ function GameView({ changeView }) {
     }
 
     // Available options
-    const [choices, setChoices] = useState([
-        {
-            src: blood,
-            onClick: () => { /* Define onClick handler for this button */ },
-            title: "Blood test",
-            dialogue: "There is nothing conclusive but indicates that scans might lead to something"
-        },
-        {
-            src: antibiotics,
-            onClick: () => { /* Define onClick handler for this button */ },
-            title: "Antibiotics"
-        },
-        {
-            src: doctor,
-            onClick: () => changeView("home"), // Example for changeView usage
-            title: "Neurologist"
-        },
-    ]);
+    const [choices, setChoices] = useState([story["BloodTest"], story["Antibiotics"], story["Neurologist"]]);
 
     return (
         <div>
