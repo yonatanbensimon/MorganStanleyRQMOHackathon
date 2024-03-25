1f6cb1ddb19a5592d49eb131e35c419af4b94929
exisodd <ethantran.mail@gmail.com>
2024-03-24 22:10:54 -0400

    removed redundant code

--- frontend/my-app/src/views/WrapperView.js
+++ frontend/my-app/src/views/WrapperView.js
@@ -9,61 +9,6 @@ import InstructionsView from "./InstructionsView/InstructionsView";
 
 function WrapperView() {
   const [currentView, setCurrentView] = useState("home");
-  const [weeks, setWeeks] = useState(0);
-  const [availableOptions, setAvailableOptions] = useState([]);
-
-  /*
-  *   Function called when game starts. Initialize the disease.
-  *   Update availableOptions array to add beginning options.
-  *   Set `weeks` to zero. Call change view with the appropriate
-  *   parameter.
-  *
-  *   @return {String} - The instructions text.
-  */
-  function init() {
-    setAvailableOptions(Story.InitialOptions);
-    setCurrentView("game");
-    setWeeks(0);
-    return "Please Select an Option to Begin the Game";
-  }
-
-  /*
-  *  Function called when a user selects an option. Update the
-  *  availableOptions array to reflect the new options. Update
-  *  the weeks. If choseOption is a winning option, call
-  *  results().
-  * 
-  *  @return {String} - The dialogue of the chosenOption.
-  */
-  function updateOptions(chosenOption) {
-    // Get the game result from the chosen option
-    const gameResult = Story?.chosenOption?.gameresult ?? "defaultResult";
-    
-    // Check if the chosen option is a winning option
-    if (gameResult) {
-      return results();
-    }
-    
-    // Remove the chosen option from the available options
-    setAvailableOptions(availableOptions.filter(option => option !== chosenOption));
-    
-    // Add the new options to the available options
-    setAvailableOptions(availableOptions += Story.chosenOption.NewOptions);
-
-    // Update the weeks
-    setWeeks(weeks + Story.chosenOption.weeks);
-  }
-
-  /*
-  *  Function called when the user wins the game. Call changeView
-  *  with the appropriate parameter.
-  * 
-  *  @return {String} - The results of the game
-  */
-  function results() {
-    setCurrentView("result");
-    return 'You guessed the disease! Congratulations!';
-  }
   
   const renderView = () => {
     switch (currentView) {
